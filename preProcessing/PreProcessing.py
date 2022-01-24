0b22d37811368436026de17c7d5b630c5046f9e6
tmbarchi <tathianabarchi@alunos.utfpr.edu.br>
2022-01-24 20:45:31 -0300

    .

--- preProcessing/PreProcessing.py
+++ preProcessing/PreProcessing.py
@@ -1,18 +1,31 @@
 from typing import Tuple, Union
 
-from pandas import concat, Series, DataFrame, Grouper
-from statsmodels.tsa.stattools import adfuller
 import numpy as np
-
+from pandas import Series, DataFrame, Grouper
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
+from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.stattools import pacf
 
+'''
+    1) Para todo o código é interessante lembrarmos
+    do operar "del" do python, pode servir muito
+    bem a você, principalmente quando não precisa
+    usar uma variável mais, que ocupe bastante espaço
+    em memória.
+    
+    2) Deixo também a dica de colocar o tipo da variável
+    que está mexendo, isso ajudará a IDE a te mostrar as
+    funções que o tipo específico tem.
+'''
+
+
 def _toStationary(series: Series) -> Tuple[Series, Union[Series, DataFrame], Union[Series, DataFrame]]:
+    # talvez o retorno aqui seja "SeriesGroupBy" e não "Union", verificar...
     monthlyMean: Union[Series, DataFrame] = series.groupby(Grouper(freq="D")).mean()
     monthlySTD: Union[Series, DataFrame] = series.groupby(Grouper(freq="D")).std()
 
+    # Aqui diz que não existe o atributo DATE pro tipo INDEX, vale a pena verificar
     for date in monthlySTD.index.date:
         filterSeries: bool = series.index.date == date
         filterMean: bool = monthlyMean.index.date == date
@@ -28,53 +41,70 @@ def _isStationary(series: Series) -> bool:
     results = adfuller(series)
     return results[1] <= 0.05
 
-def verify_toStationary(serie: Series):
 
+def verify_toStationary(serie: Series):
     scalerSTD, scalerMean = None, None
 
     if not _isStationary(serie):
         print("Aplicando Z-Score")
-
         serie, scalerSTD, scalerMean = _toStationary(serie)
 
     return serie, scalerSTD, scalerMean
 
-def featureSelectionCorr(serie):
 
+# Alterar o parâmetro para ter o tipo específico (serie: Series)
+# Colocar o tipo do retorno é interessante também
+def featureSelectionCorr(serie):
     data = DataFrame(index=serie.index)
 
     for i in range(1, 10 + 1):
         data["lag_" + str(i)] = serie.shift(i)
 
+    # Não entendi muito bem o que a linha
+    # abaixo tem como objetivo...
     data = data[10:]
 
     data["actual"] = serie
 
+    # talvez trocar a linha abaixo para "corr: list"
     corr = {}
-    for column in data.loc[:, data.columns!="actual"]:
+    for column in data.loc[:, data.columns != "actual"]:
         corr[column] = np.abs(data[column].corr(data["actual"], method="spearman"))
+
     sortedCorr = sorted(corr.items(), key=lambda x: x[1], reverse=True)
+
     filter = list(dict(sortedCorr).keys())[:5]
     print("\t" + str(filter))
+
     return filter
 
+
+# Alterar o parâmetro para ter o tipo específico (serie: Series)
 def featureSelectionPACF(serie):
     pacf_values, confint = pacf(serie, nlags=30, alpha=0.05)
-    lower_bound = confint[1,0] - pacf_values[1]
-    upper_bound = confint[1,1] - pacf_values[1]
+    lower_bound = confint[1, 0] - pacf_values[1]
+    upper_bound = confint[1, 1] - pacf_values[1]
+    print("Lower_bound, upper_bound:")
     print("\t", lower_bound, upper_bound)
+
     significant_pacf_values = {}
     for i in range(1, len(pacf_values)):
         if pacf_values[i] >= upper_bound or pacf_values[i] <= lower_bound:
             significant_pacf_values[i] = pacf_values[i]
+
     sorted_significant_pacf_values = sorted(significant_pacf_values.items(), key=lambda x: x[1], reverse=True)
     if len(sorted_significant_pacf_values) >= 10:
         sorted_significant_pacf_values = sorted_significant_pacf_values[:10]
+
     significant_lags = [a_tuple[0] for a_tuple in sorted_significant_pacf_values]
     significant_lags.sort()
+    print("Significant lags: ")
     print("\t" + str(significant_lags))
+
     return significant_lags
 
+
+# Alterar o parâmetro para ter o tipo específico (serie: Series, ...)
 def generate_X(serie, lags):
     data = DataFrame()
     data["actual"] = serie
@@ -82,8 +112,10 @@ def generate_X(serie, lags):
         data["lag_" + str(lag)] = serie.shift(lag)
     return data[lags[-1]:]
 
-def prepareDataToANN(serie):
 
+# Alterar o parâmetro para ter o tipo específico (serie: Series)
+# Talvez renomear para prepareToANN seja suficiente
+def prepareDataToANN(serie):
     train, valTest = train_test_split(serie, train_size=0.6, shuffle=False)
     validation, test = train_test_split(valTest, test_size=0.5, shuffle=False)
 
@@ -93,46 +125,69 @@ def prepareDataToANN(serie):
 
     lags = featureSelectionPACF(train)
 
+    '''
+        Aqui poderia fazer um por um, chamando uma função...
+        Ou seja, gerar, setar, pegar informações e setar...
+            EX:
+              - Gerar: "generate_X(train, lags)"
+              - Setar: "
+                    columns = train.columns != "actual"
+                    processedTrain = scalerTrain.fit_transform(train.loc[:, columns])
+                    dfProcessedTrain = DataFrame(data=processedTrain, index=train.index, columns=train.loc[:, columns].columns)
+                    dfProcessedTrain["actual"] = train["actual"]
+              "
+              - Pegar informações, como o "columns"
+              - Retornar os valores que são importantes a você, que acredito ser "dfProcessedTrain"
+              - Repetir para cada um: Train, Validation, Test
+            (Isso fará que você use menos memória, deixando mais performático)
+    '''
     train = generate_X(train, lags)
     validation = generate_X(validation, lags)
     test = generate_X(test, lags)
 
-    minMaxVal = (validation.to_numpy().min(), validation.to_numpy().max())
-    minMaxTest = (test.to_numpy().min(), test.to_numpy().max())
+    # Tentar tirar essas conversões em memória (to_numpy()) verificando
+    # se o tipo retornado já não tem uma função pra isso. Lendo a documentação
+    # acredito que consegui pegar uma dica...
+    minMaxVal = (validation.values.min(), validation.values.max())
+    minMaxTest = (test.values.min(), test.values.max())
 
+    # Não daria pra usar somente um "MinMaxScaler(feature_range=(-1, 1))"???
     scalerTrain = MinMaxScaler(feature_range=(-1, 1))
     scalerVal = MinMaxScaler(feature_range=(-1, 1))
     scalerTest = MinMaxScaler(feature_range=(-1, 1))
 
     columns = train.columns != "actual"
 
-    processedTrain = scalerTrain.fit_transform(train.loc[:, columns])
+    processedTrain = scalerTrain.fit_transform(train)
     processedVal = scalerVal.fit_transform(validation.loc[:, columns])
     processedTest = scalerTest.fit_transform(test.loc[:, columns])
 
-    dfProcessedTrain = DataFrame(data=processedTrain, index=train.index, columns=train.loc[:, columns].columns)
+    dfProcessedTrain = DataFrame(data=processedTrain, index=train.index, columns=train.columns)
     dfProcessedVal = DataFrame(data=processedVal, index=validation.index, columns=validation.loc[:, columns].columns)
     dfProcessedTest = DataFrame(data=processedTest, index=test.index, columns=test.loc[:, columns].columns)
 
-    dfProcessedTrain["actual"] = train["actual"]
     dfProcessedVal["actual"] = validation["actual"]
     dfProcessedTest["actual"] = test["actual"]
 
     return dfProcessedTrain, dfProcessedVal, dfProcessedTest, minMaxVal, minMaxTest
 
-def prepareDataToLinearModels(serie):
 
+# Alterar o parâmetro para ter o tipo específico (serie: Series)
+# Talvez trocar o nome da função, tentando reduzí-la (ex: prepareToLM)
+#     - A omissão de DATA pois você já estará passando por parâmetro
+#     - A alteração de LinearModels por LM por convenção do código
+def prepareDataToLinearModels(serie: Series):
     train, test = train_test_split(serie, train_size=0.6, shuffle=False)
 
-    minMaxTest = (test.to_numpy().min(), test.to_numpy().max())
+    minMaxTest = (test.values.min(), test.values.max())
 
     scalerTrain = MinMaxScaler(feature_range=(-1, 1))
     scalerTest = MinMaxScaler(feature_range=(-1, 1))
 
-    processedTrain = scalerTrain.fit_transform(train.values.reshape(-1,1))
-    processedTest = scalerTest.fit_transform(test.values.reshape(-1,1))
+    processedTrain = scalerTrain.fit_transform(train.values.reshape(-1, 1))
+    processedTest = scalerTest.fit_transform(test.values.reshape(-1, 1))
 
-    dfProcessedTrain = DataFrame(data=processedTrain, index=train.index)
-    dfProcessedTest = DataFrame(data=processedTest, index=test.index)
+    dfProcessedTrain = Series(data=processedTrain.reshape(1,-1)[0], index=train.index)
+    dfProcessedTest = Series(data=processedTest.reshape(1,-1)[0], index=test.index)
 
     return dfProcessedTrain, dfProcessedTest, minMaxTest
